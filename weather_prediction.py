d4ac6d9888e3dccfb66c9f04ae95796dab0d5fd2
Bowen Yang <boweny@sfu.ca>
2017-07-31 18:00:23 -0700

    before merge

--- weather_prediction.py
+++ weather_prediction.py
@@ -1,1 +1,9 @@
-#weather prediction
+
+import numpy as np
+import pandas as pd
+from skimage.io import imread_collection
+
+
+#load image: reference: https://stackoverflow.com/questions/30230592/loading-all-images-using-imread-from-a-given-folder
+img_dir = 'katkam-scaled/*.jpg'
+img_list = col = imread_collection(img_dir)
