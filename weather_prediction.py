261b2bfc7ee75f882eaf40549fbfe30419f07479
xta13 <xta13@sfu.ca>
2017-08-01 08:54:42 +0800

    loaded images

--- weather_prediction.py
+++ weather_prediction.py
@@ -1,1 +1,33 @@
+
+# coding: utf-8
+
+# In[1]:
+
 #weather prediction
+import pandas as pd
+import numpy as np
+from scipy import misc
+from skimage.io import imread_collection
+pictures = np.array(imread_collection('katkam-scaled/*.jpg'))
+
+
+# In[14]:
+
+shape = pictures.shape
+pictures
+
+
+# In[15]:
+
+X = pictures.reshape(shape[0],shape[1] * shape[2] * shape[3])
+
+
+# In[13]:
+
+print(X[1])
+
+
+# In[ ]:
+
+
+
